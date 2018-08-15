1f51ca8894d6a4a28be6fcbe6afad7ea1620262f
Martin Schwinzerl <martin.schwinzerl@gmail.com>
2018-08-15 18:43:35 +0200

    first complete iteration of the LHC export script

--- examples/lhc/export_to_cobjects.py
+++ examples/lhc/export_to_cobjects.py
@@ -1,144 +1,112 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sixtracktools
 import pysixtrack
 from cobjects import CBuffer, CObject, CField
 
-import pdb
-
-def factorial( x ):
-    if  not isinstance( x, ( int ) ):
-        return 0
-    return x > 0 and x * factorial( x - 1 ) or 1
-
-class Drift( CObject ):
-    _typeid =  2
-    length  = CField( 0, 'real', default=0.0, alignment=8 )
-
-class DriftExact( CObject ):
-    _typeid = 3
-    length  = CField( 0, 'real', default=0.0, alignment=8 )
-
-class MultiPole( CObject ):
-    _typeid = 4
-    length  = None
-    hxl     = None
-    hyl     = None
-    order   = None
-    bal     = None
-
-    def __init__( self, order = 1, cbuffer=None, **kwargs ):
-        CObject.__init__( self, cbuffer=cbuffer, **kwargs )
-
-        self.length = CField( 0, 'real',    default=0.0,  alignment=8 )
-        self.hxl    = CField( 1, 'real',    default=0.0,  alignment=8 )
-        self.hyl    = CField( 2, 'real',    default=0.0,  alignment=8 )
-        self.order  = CField( 3, 'int64',   default=0,    alignment=8 )
-        self.bal    = CField( 4, 'real',    default=0.0,
-                                 length=( 2 * order + 2 ),
-                                 pointer=True, alignment=8 )
-        self.order  = order
-
-
-class Cavity( CObject ):
-    _typeid = 5
-    voltage   = CField( 0, 'real', default=0.0,  alignment=8 )
-    frequency = CField( 1, 'real', default=0.0,  alignment=8 )
-    lag       = CField( 2, 'real', default=0.0,  alignment=8 )
-
-class XYShift( CObject ):
-    _typeid = 6
-    dx      = CField( 0, 'real',   default=0.0,  alignment=8 )
-    dy      = CField( 1, 'real',   default=0.0,  alignment=8 )
-
-class SRotation( CObject ):
-    _typeid = 7
-    cos_z   = CField( 0, 'real',   default=1.0,  alignment=8 )
-    sin_z   = CField( 1, 'real',   default=0.0,  alignment=8 )
-
-class BeamBeam4D( CObject ):
-    pass
-
-class BeamBeam6D( CObject ):
-    pass
-
+from beam_elements import Drift, DriftExact, MultiPole, Cavity, XYShift, SRotation
+from beam_elements import BeamBeam4D, BeamBeam6D
+from particles     import Particles as IOParticles
 
 if  __name__ == '__main__':
     from math import pi
     from math import sin, cos
     import numpy as np
+    import pdb
+
+    deg2rad = pi / 180.0
+
+    # -------------------------------------------------------------------------
+    # Dump beam elements:
 
     six = sixtracktools.SixTrackInput('.')
     line, rest, iconv = six.expand_struct(convert=pysixtrack.element_types)
-    deg2rad = pi / 180.0
 
-    b = CBuffer()
+    beam_elements = CBuffer()
 
     for label, elem_type, elem in line:
         if  elem_type == 'Drift':
-            e = Drift( cbuffer=b )
-            e.length = elem.length
+            e = Drift( cbuffer=beam_elements, length=elem.length )
 
         elif elem_type == 'DriftExact':
-            e = DriftExact( cbuffer=b )
-            e.length = elem.length
+            e = DriftExact( cbuffer=beam_elements, length=elem.length )
 
         elif elem_type == 'Multipole':
-            order = max( len( elem.knl ), len( elem.ksl ) )
-
-            assert( order >= len( elem.knl ) )
-            assert( order >= len( elem.ksl ) )
-
-            knl = [ 0.0 for x in range( order ) ]
-            knl[ :len( elem.knl ) ] = elem.knl
-
-            ksl = [ 0.0 for x in range( order ) ]
-            ksl[ :len( elem.ksl ) ] = elem.ksl
-
-            bal = np.zeros( 2 * order )
-
-            for i in range( order ):
-                inv_factorial = float( 1.0 / factorial( i ) )
-                bal[ 2 * i     ] = knl[ i ] * inv_factorial
-                bal[ 2 * i + 1 ] = ksl[ i ] * inv_factorial
-
-            if  order > 0: order -= 1
-
-            e = MultiPole( order=order, cbuffer=b )
-            e.length = elem.length
-            e.hxl    = elem.hxl
-            e.hyl    = elem.hyl
-            e.order  = order
-            e.bal    = bal
-
+            e = MultiPole( cbuffer=beam_elements, knl=elem.knl, ksl=elem.ksl,
+                           length=elem.length, hxl=elem.hxl, hyl=elem.hyl )
 
         elif elem_type == 'XYShift':
-            e = XYShift( cbuffer=b )
-            e.dx = elem.dx
-            e.dy = elem.dy
-
+            e = XYShift( cbuffer=beam_elements, dx=elem.dx, dy=elem.dy )
 
         elif elem_type == 'SRotation':
             angle_rad = deg2rad * elem.angle
-            cos_z     = cos( angle_rad )
-            sin_z     = sin( angle_rad )
-            e = SRotation( cbuffer=b )
-            e.cos_z = cos_z
-            e.sin_z = sin_z
-
+            e = SRotation( cbuffer=beam_elements,
+                           cos_z=cos( angle_rad ), sin_z=sin( angle_rad ) )
 
         elif elem_type == 'Cavity':
-            e = Cavity( cbuffer=b )
-            e.voltage   = elem.voltage
-            e.frequency = elem.frequency
-            e.lag       = elem.lag
+            e = Cavity( cbuffer=beam_elements, voltage=elem.voltage,
+                        frequency=elem.frequency, lag=elem.lag )
 
         else:
-            print( elem_type )
+            print( "Unknown/unhandled element type: {0}".format( elem_type, ) )
             pdb.set_trace()
 
-    b.to_file( './lhc_beam_elements.bin' )
+    # -------------------------------------------------------------------------
+    # Dump particles (element by element)
 
+    sixdump = sixtracktools.SixDump101('res/dump3.dat')
 
+    num_iconv = int( len( iconv ) )
+    num_belem = int( len( line  ) )
+    num_dumps = int( len( sixdump ) )
 
+    assert(   num_iconv >  0 )
+    assert(   num_belem >  iconv[ num_iconv - 1 ]  )
+    assert(   num_dumps >= num_iconv )
+    assert( ( num_dumps %  num_iconv ) == 0 )
+
+    num_particles = int( num_dumps / num_iconv )
+
+    particles_buffer = CBuffer()
+
+    for ii in range( num_iconv ):
+        elem_id = iconv[ ii ]
+        assert( elem_id < num_belem )
+
+        p  = IOParticles( cbuffer=particles_buffer,
+                          num_particles=num_particles )
+
+        assert( p.num_particles == num_particles )
+        assert( len( p.q0 ) == num_particles )
+
+        for jj in range( num_particles ):
+            kk = num_particles * ii + jj
+            assert( kk < num_dumps )
+
+            inp = pysixtrack.Particles( **sixdump[ kk ].get_minimal_beam() )
+            p.q0[ jj ]         = inp._q0
+            p.mass0[ jj ]      = inp.mass0
+            p.beta0[ jj ]      = inp.beta0
+            p.gamma0[ jj ]     = inp.gamma0
+            p.p0c[ jj ]        = inp.p0c
+            p.s[ jj ]          = inp.s
+            p.x[ jj ]          = inp.x
+            p.y[ jj ]          = inp.y
+            p.px[ jj ]         = inp.px
+            p.py[ jj ]         = inp.py
+            p.zeta[ jj ]       = inp.zeta
+            p.psigma[ jj ]     = inp.psigma
+            p.delta[ jj ]      = inp.delta
+            p.rpp[ jj ]        = inp.rpp
+            p.rvv[ jj ]        = inp.rvv
+            p.chi[ jj ]        = inp.chi
+            p.particle[ jj ]   = inp.partid is not None and inp.partid or jj
+            p.at_element[ jj ] = elem_id
+            p.at_turn[ jj ]    = 0
+            p.state[ jj ]      = inp.state
+
+    particles_buffer.to_file( './lhc_particle_dump.bin' )
 
 
 
