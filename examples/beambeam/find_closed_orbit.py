fd3451f06230b9620414c7d6d067f5a37a573ef3
giadarol <giovanni.iadarola@cern.ch>
2018-08-18 21:37:13 +0200

    Closed orbit found

--- examples/beambeam/find_closed_orbit.py
+++ examples/beambeam/find_closed_orbit.py
@@ -3,6 +3,8 @@ import pysixtrack
 import pysixtrack.helpers as hp
 from pysixtrack.particles import Particles
 
+import scipy.optimize as so
+
 import numpy as np
 
 six = sixtracktools.SixTrackInput('.')
@@ -24,13 +26,13 @@ class Ring(pysixtrack.Line):
 
     def one_turn_map(self, coord):
 
-        pcl=Particles(p0c=self.p0c_eV)
+        pcl=Particles(p0c=self.p0c)
 
         pcl.x = coord[0]
         pcl.px = coord[1]
         pcl.y = coord[2]
         pcl.py = coord[3]
-        pcl.sigma = coord[4]
+        pcl.zeta = coord[4]
         pcl.delta = coord[5]
 
         self.track(pcl)
@@ -39,50 +41,39 @@ class Ring(pysixtrack.Line):
 
         return coord_out
 
+    def _CO_error(self, coord):
+        return np.sum((self.one_turn_map(coord)-coord)**2)
+
+    def find_closed_orbit(self, guess=np.array([0.,0.,0.,0.,0.,0.])):
+        res = so.minimize(self._CO_error, guess, tol=1e-20, method='Nelder-Mead')
+
+        pcl=Particles(p0c=self.p0c)
+
+        pcl.x = res.x[0]
+        pcl.px = res.x[1]
+        pcl.y = res.x[2]
+        pcl.py = res.x[3]
+        pcl.zeta = res.x[4]
+        pcl.delta = res.x[5]
+
+        CO = self.track_elem_by_elem(pcl)
+
+        return CO
+
 ring = Ring(line, p0c=p0c_eV)
 
-prrrr
+closed_orbit = ring.find_closed_orbit()
 
-# Build 1-turn map function
-def one_turn_map(coord_in, flag_ebe=False):
+x_CO = np.array([pp.x for pp in closed_orbit])
+y_CO = np.array([pp.y for pp in closed_orbit])
 
-    # coord = np.array([x, px, y, py, sigma, delta])
-
-    coord = coord_in
-    
-    npart = 1
-    
-    bunch=sixtracklib.CParticles(npart=npart, 
-            p0c=p0c_eV,
-            beta0 = beta0,
-            gamma0 = gamma0)
-    bunch.x+=coord[0]
-    bunch.px+=coord[1]
-    bunch.y+=coord[2]
-    bunch.py+=coord[3]
-    bunch.sigma+=coord[4]
-    bunch.set_delta(coord[5])
-
-    particles,ebe,tbt = machine.track_cl(bunch,nturns=1,elembyelem={True:True, False:None}[flag_ebe], turnbyturn=True)
-
-    coord =  np.array([tbt.x[1][0], tbt.px[1][0], tbt.y[1][0], tbt.py[1][0], 
-                    tbt.sigma[1][0], tbt.delta[1][0]])
-
-    if flag_ebe:
-        return coord, ebe
-    else:
-        return coord
-
-# Define function for optimization
-tominimize = lambda coord: np.sum((one_turn_map(coord)-coord)**2)
-
-# Find fixed point
-res = so.minimize(tominimize, np.array([0.,0.,0.,0.,0.,0.]), tol=1e-20, method='Nelder-Mead')
-# Get close orbit around the machine
-temp, ebe_CO = one_turn_map(res.x, flag_ebe=True)
-
-
-# sixdump_all = sixtracktools.SixDump101('res/dump3.dat')
-
-
-
+# Compare closed orbit against sixtrack
+# Load sixtrack tracking data
+sixdump_all = sixtracktools.SixDump101('res/dump3.dat')
+# Assume first particle to be on the closed orbit
+Nele_st = len(iconv)
+sixdump_CO = sixdump_all[::2][:Nele_st]
+x_CO_at_st_ele = x_CO[iconv]
+y_CO_at_st_ele = y_CO[iconv]
+print('Max C.O. discrepancy in x %.2e m'%np.max(np.abs(x_CO_at_st_ele-sixdump_CO.x)))
+print('Max C.O. discrepancy in y %.2e m'%np.max(np.abs(y_CO_at_st_ele-sixdump_CO.y)))
