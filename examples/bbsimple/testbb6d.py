36a04f2bb9e137ba67ea41e483aebc087b3b8aea
giadarol <giovanni.iadarola@cern.ch>
2018-08-31 16:31:36 +0200

    Working with one slice but not with several slices

--- examples/bbsimple/testbb6d.py
+++ examples/bbsimple/testbb6d.py
@@ -18,9 +18,37 @@ for bb in listBB4D: bb.enabled = False
 ind_BB6D, namelistBB6D, listBB6D = hp.get_elems_of_type(line, 'BeamBeam6D')
 for bb in listBB6D: bb.BB6D_data.enabled = False
 
+# Load sixtrack tracking data
+sixdump_all = sixtracktools.SixDump101('res/dump3.dat')
+# Assume first particle to be on the closed orbit
+Nele_st = len(iconv)
+sixdump_CO = sixdump_all[::2][:Nele_st]
+
 # Find closed orbit
 ring = hp.Ring(line, p0c=p0c_eV)
-closed_orbit = ring.find_closed_orbit()
+guess = [getattr(sixdump_CO, att)[0] for att in 'x px y py sigma delta'.split()]
+closed_orbit = ring.find_closed_orbit(guess=guess, method='get_guess')
+
+# Check that closed orbit is closed
+pstart = closed_orbit[0].copy()
+pstart_st = pysixtrack.Particles(**sixdump_CO[0].get_minimal_beam())
+print('STsigma, Sigma, Stdelta, delta, Stpx, px')
+for iturn in range(10):
+	ring.track(pstart)
+	ring.track(pstart_st)
+	print('%e, %e, %e, %e, %e, %e'%(pstart_st.sigma, pstart.sigma, pstart_st.delta, pstart.delta, pstart_st.px, pstart.px) )
+
+import matplotlib.pyplot as plt
+
+# Compare closed orbit against sixtrack
+for att in 'x px y py delta sigma'.split():
+	att_CO = np.array([getattr(pp, att) for pp in closed_orbit])
+	att_CO_at_st_ele = att_CO[iconv]
+	print('Max C.O. discrepancy in %s %.2e'%(att, np.max(np.abs(att_CO_at_st_ele-getattr(sixdump_CO, att)))))
+
+plt.figure(1)
+plt.plot(sixdump_CO.s, sixdump_CO.x)
+
 
 # Re-enable beam-beam
 for bb in listBB4D: bb.enabled = True
@@ -57,8 +85,6 @@ for bb, ibb in zip(listBB6D, ind_BB6D):
 
 
 
-
-
 # Evaluate kick at CO location BB6D
 for bb, ibb in zip(listBB6D, ind_BB6D):
 
@@ -85,7 +111,6 @@ for bb, ibb in zip(listBB6D, ind_BB6D):
     bb.BB6D_data.Ddelta_sub = ptemp.delta - ptempin.delta  
 
 # Check that the closed orbit is not kicked
-
 for bb, ibb in zip(listBB6D, ind_BB6D):
 
     ptemp = closed_orbit[ibb].copy()
@@ -98,24 +123,31 @@ for bb, ibb in zip(listBB6D, ind_BB6D):
 
 
 
-# Compare closed orbit against sixtrack
-# Load sixtrack tracking data
-sixdump_all = sixtracktools.SixDump101('res/dump3.dat')
-# Assume first particle to be on the closed orbit
-Nele_st = len(iconv)
-sixdump_CO = sixdump_all[::2][:Nele_st]
-x_CO = np.array([pp.x for pp in closed_orbit])
-y_CO = np.array([pp.y for pp in closed_orbit])
-x_CO_at_st_ele = x_CO[iconv]
-y_CO_at_st_ele = y_CO[iconv]
-print('Max C.O. discrepancy in x %.2e m'%np.max(np.abs(x_CO_at_st_ele-sixdump_CO.x)))
-print('Max C.O. discrepancy in y %.2e m'%np.max(np.abs(y_CO_at_st_ele-sixdump_CO.y)))
-
-
 # Compare tracking results
 sixdump = sixdump_all[1::2] # Particle with deviation from CO
-# sixdump = sixdump_all[::2] # Particle on CO
+#sixdump = sixdump_all[::2] # Particle on CO
 
+p_in_st = pysixtrack.Particles(**sixdump[0].get_minimal_beam())
+p_out_st = pysixtrack.Particles(**sixdump[1].get_minimal_beam())
+
+p_in_pyst = p_in_st.copy()
+p_out_pyst = p_in_pyst.copy()
+
+listBB6D[0].track(p_out_pyst)
+
+
+for att in 'x px y py delta sigma'.split():
+    attin = getattr(p_in_st, att)
+    attout = getattr(p_out_st, att)
+    print('SxTr: Change in '+att+': %e'%(attout-attin))
+
+    attin_pyst = getattr(p_in_pyst, att)
+    attout_pyst = getattr(p_out_pyst, att)
+    print('PyST: Change in '+att+': %e'%(attout_pyst-attin_pyst))
+
+
+
+'''
 def compare(prun,pbench):
     out=[]
     for att in 'x px y py delta sigma'.split():
@@ -147,3 +179,4 @@ for ii in range(1,len(iconv)):
     if out>1e-13:
         print("Too large discrepancy")
         break
+'''
