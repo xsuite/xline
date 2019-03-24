ac00c8effdfcc1458a6b6a630612fe94cdb67838
Hannes Bartosik <hannes.bartosik@cern.ch>
2019-03-24 06:59:37 +0100

    Frist version of SC_controller class.

--- examples/spacecharge/000_prepare_spacecharge_lattice.py
+++ examples/spacecharge/000_prepare_spacecharge_lattice.py
@@ -7,9 +7,134 @@ import numpy as np
 import matplotlib.pylab as plt
 import pickle
 
+class SC_controller(object):
+
+    def __init__(self,type,intensity,eps_x,eps_y,dpp_rms,
+                    min_sigma_diff=1e-1,bunchlength=None):
+        assert(type in ['SpaceChargeCoast'])
+        self._listSCnodes = []
+        self._min_sigma_diff = min_sigma_diff
+        self._type = type
+        self.setBunchParameters(intensity,eps_x,eps_y,dpp_rms,bunchlength)
+    
+    def installSCnodes(self,lattice,twiss,min_distance):
+        assert (len(twiss['s']) == len(lattice))
+        gamma = twiss['param']['gamma']
+        beta = np.sqrt(1.-1./gamma**2)
+        self._circumference = twiss['param']['length']
+
+        circumference = self._circumference
+        intensity = self._intensity
+        eps_x = self._eps_x
+        eps_y = self._eps_y
+        dpp_rms = self._dpp_rms
+        bunchlength = self._bunchlength
+
+        new_lattice = []
+        s_lastSCkick = 0
+        for i, e in enumerate(lattice):
+            s = twiss['s'][i]
+            new_lattice.append(e)
+            if s-s_lastSCkick > min_distance or i==(len(elems)-1):
+                beta_x = twiss['betx'][i]
+                beta_y = twiss['bety'][i]
+                D_x = twiss['dx'][i]
+                D_y = twiss['dy'][i]
+                CO_x = twiss['x'][i]
+                CO_y = twiss['y'][i]
+
+                sigma_x = self._sigma(beta_x, eps_x, D_x, dpp_rms)
+                sigma_y = self._sigma(beta_y, eps_y, D_y, dpp_rms)
+                length = s-s_lastSCkick
+                if self._type == 'SpaceChargeCoast':
+                    scNode = pysixtrack.SpaceChargeCoast(
+                        line_density=intensity/circumference,
+                        sigma_x=sigma_x,
+                        sigma_y=sigma_y,
+                        length=length,
+                        min_sigma_diff=self._min_sigma_diff,
+                        Delta_x=CO_x,
+                        Delta_y=CO_y,
+                        enabled=True)
+                new_lattice.append(('SC%i'%(len(self._listSCnodes)), 'SpaceChargeCoast', scNode))
+                s_lastSCkick = s
+
+                optics = {}
+                optics['beta_x'] = beta_x
+                optics['beta_y'] = beta_y
+                optics['D_x'] = D_x
+                optics['D_y'] = D_y
+                optics['s'] = s
+                self._addNode(optics, scNode)
+        
+        # to verify that integrated length of SC kicks corresponds to circumference
+        lengthsSCndodes = self.getSCnodesLengths()
+        assert(self._circumference == sum(lengthsSCndodes))
+        print("\n  installed %d space charge kicks"%len(self._listSCnodes))
+        print("\n  maximum length of space charge kicks: %1.2f m"%(max(lengthsSCndodes)))
+        print("\n  average length of space charge kicks: %1.2f m"%(np.mean(lengthsSCndodes)))
+        print("\n  integrated length of space charge kicks: %1.2f m"%(sum(lengthsSCndodes)))
+
+        return new_lattice
+
+    def getSCnodesOpticsParameter(self,p):
+        return [optics[p] for optics in self.getSCnodes()[1]]
+
+    # def setSCnodesOpticsParameter(self,p,newValues):
+    #     assert(len(newValues) == len(self._listSCnodes))
+    #     for i, optics in enumerate(self.getSCnodes()[1]):
+    #         optics[p] = newValues[i]
+    
+    def setClosedOrbit(self):
+        print('to be implemented')
+
+    def _sigma(self, beta, eps, D, dpp_rms):
+        return np.sqrt(beta*eps + D**2*dpp_rms**2)
+    
+    def _addNode(self, node, optics):
+        self._listSCnodes.append((optics, node))
+    
+    def getSCnodes(self):
+        # nodes, optics = list(zip(*self.listSCnodes))
+        nodes = [sc[0] for sc in self._listSCnodes]
+        optics = [sc[1] for sc in self._listSCnodes]
+        return nodes, optics
+
+    def getSCnodesLengths(self):
+        nodes, _ = self.getSCnodes()
+        return [n.length for n in nodes]
+    
+    def disableSCnodes(self):
+        nodes, _ = self.getSCnodes()
+        for n in nodes: n.enabled = False
+    
+    def enableSCnodes(self):
+        nodes, _ = self.getSCnodes()
+        for n in nodes: n.enabled = True
+    
+    def setBunchParameters(self,intensity,eps_x,eps_y,dpp_rms,bunchlength=None):
+        self._intensity = intensity
+        self._eps_x = eps_x
+        self._eps_y = eps_y
+        self._dpp_rms = dpp_rms
+        self._bunchlength = bunchlength
+        if self._listSCnodes:
+            for node, optics in zip(self.getSCnodes()):
+                beta_x = optics['beta_x']
+                beta_y = optics['beta_y']
+                D_x = optics['D_x']
+                D_y = optics['D_y']
+
+                node.sigma_x = self._sigma(beta_x, eps_x, D_x, dpp_rms)
+                node.sigma_y = self._sigma(beta_y, eps_y, D_y, dpp_rms)
+                if self._type == 'SpaceChargeCoast':
+                    node.line_density = intensity/self._circumference
+
+
+
 #see sps/madx/a001_track_thin.madx
 mad=madlang.open('madx/SPS_Q20_thin.seq')
-mad.acta_31637.volt=0 #4.5
+mad.acta_31637.volt=4.5
 mad.acta_31637.lag=0.5
 
 
@@ -17,12 +142,7 @@ elems,rest,iconv=mad.sps.expand_struct(pysixtrack.element_types)
 sps=pysixtrack.Line(elements= [e[2] for e in elems])
 spstwiss=tfs.open('madx/twiss_SPS_Q20_thin.tfs')
 
-gamma = spstwiss['param']['gamma']
-beta = np.sqrt(1.-1./gamma**2)
-betagamma = beta*gamma
-
-
-# remove beg. and end. markers in twiss table
+# remove beg. and end. markers in twiss table (as they are not in sixtrack lattice)
 for k in spstwiss:
     try:
         spstwiss[k] = spstwiss[k][1:-1]
@@ -30,7 +150,60 @@ for k in spstwiss:
         pass
 assert (len(spstwiss['s']) == len(elems))
 
+gamma = spstwiss['param']['gamma']
+beta = np.sqrt(1.-1./gamma**2)
+betagamma = beta*gamma
 
+p0c = 25.92e9
+intensity=2e11 * spstwiss['param']['length'] # for a coasting beam with a line density of 2e11/m
+eps_x=2e-6/betagamma
+eps_y=2e-6/betagamma
+dpp_rms=1.5e-3
+
+my_SC_controller = SC_controller('SpaceChargeCoast',intensity,eps_x,eps_y,dpp_rms)
+new_elems = my_SC_controller.installSCnodes(elems,spstwiss,min_distance=25.)
+
+# prepare a particle on the closed orbit
+p=Particles(p0c=p0c)
+ring = hp.Ring(new_elems, p0c=p0c)
+
+my_SC_controller.disableSCnodes()
+# print("\n  starting closed orbit search ... ")
+closed_orbit = ring.find_closed_orbit(guess=[spstwiss['x'][0], spstwiss['px'][0], 
+    spstwiss['y'][0], spstwiss['py'][0], 0., 0.], method='get_guess')
+my_SC_controller.enableSCnodes()
+
+with open('particle_on_CO.pkl', 'wb') as fid:
+    closed_orbit[0]._m = None # to be sorted out 
+    pickle.dump(closed_orbit[0], fid)
+with open('line.pkl', 'wb') as fid:
+    pickle.dump(new_elems, fid)
+with open('SCcontroller.pkl', 'wb') as fid:
+    pickle.dump(my_SC_controller, fid)
+
+
+
+plt.close('all')
+
+f, ax = plt.subplots()
+ax.hist(my_SC_controller.getSCnodesLengths(), 100)
+ax.set_xlabel('length of SC kick (m)')
+ax.set_ylabel('counts')
+plt.show()
+
+f, ax = plt.subplots(figsize=(14,5))
+ax.plot(spstwiss['s'], spstwiss['betx'], 'b', label='x', lw=2)
+ax.plot(spstwiss['s'], spstwiss['bety'], 'g', label='y', lw=2)
+for s in my_SC_controller.getSCnodesOpticsParameter('s'): ax.axvline(s, linewidth=1, color='r', linestyle='--')
+ax.set_xlim(0,1100)
+ax.set_ylim(0,120)
+ax.set_xlabel('s (m)')
+ax.set_ylabel('beta functions (m)')
+ax.legend(loc=3)
+plt.show()
+
+
+'''
 p0c = 25.92e9
 line_density = 2e11
 epsn_x = 2e-6
@@ -38,13 +211,13 @@ epsn_y = 2e-6
 dpp_rms = 1.5e-3
 
 s_lastSCkick = 0
-s_SCkicks = [0]
+s_SCkicks = []
 min_distance = 25. #5.
-new_element_list = []
+new_elems = []
 listSC = []
 for i, e in enumerate(elems):
     s = spstwiss['s'][i]
-    new_element_list.append(e)
+    new_elems.append(e)
     if s-s_lastSCkick > min_distance or i==(len(elems)-1):
         beta_x = spstwiss['betx'][i]
         beta_y = spstwiss['bety'][i]
@@ -60,11 +233,10 @@ for i, e in enumerate(elems):
             Delta_x=spstwiss['x'][i],
             Delta_y=spstwiss['y'][i],
             enabled=True))
-        new_element_list.append(newSC)
+        new_elems.append(newSC)
         listSC.append(newSC)
         s_SCkicks.append(s)
         s_lastSCkick = s
-
 
 lengthsSC = [sc[-1].length for sc in listSC]
 
@@ -79,7 +251,7 @@ print("\n  integrated length of space charge kicks: %1.2f m"%(sum(lengthsSC)))
 for sc in listSC:
     sc[-1].enabled = False
 p=Particles(p0c=p0c)
-ring = hp.Ring(new_element_list, p0c=p0c)
+ring = hp.Ring(new_elems, p0c=p0c)
 # print("\n  starting closed orbit search ... ")
 closed_orbit = ring.find_closed_orbit(guess=[spstwiss['x'][0], spstwiss['px'][0], 
     spstwiss['y'][0], spstwiss['py'][0], 0., 0.], method='get_guess')
@@ -90,22 +262,22 @@ with open('particle_on_CO.pkl', 'wb') as fid:
     closed_orbit[0]._m = None # to be sorted out 
     pickle.dump(closed_orbit[0], fid)
 with open('line.pkl', 'wb') as fid:
-    pickle.dump(new_element_list, fid)
+    pickle.dump(new_elems, fid)
 
 
 plt.close('all')
 
 f, ax = plt.subplots()
 ax.hist(lengthsSC, 100)
-ax.set_xlabel('length of SC kick')
+ax.set_xlabel('length of SC kick (m)')
 ax.set_ylabel('counts')
 plt.show()
 
 
 f, ax = plt.subplots(figsize=(14,5))
-ax.plot(spstwiss['s'], spstwiss['betx'], 'b', label='x')
-ax.plot(spstwiss['s'], spstwiss['bety'], 'g', label='y')
-for s in s_SCkicks: ax.axvline(s, linewidth=0.5, color='r')
+ax.plot(spstwiss['s'], spstwiss['betx'], 'b', label='x', lw=2)
+ax.plot(spstwiss['s'], spstwiss['bety'], 'g', label='y', lw=2)
+for s in s_SCkicks: ax.axvline(s, linewidth=1, color='r', linestyle='--')
 ax.set_xlim(0,1100)
 ax.set_ylim(0,120)
 ax.set_xlabel('s (m)')
@@ -113,63 +285,10 @@ ax.set_ylabel('beta functions (m)')
 ax.legend(loc=3)
 plt.show()
 
-
 '''
 
 
 
-ring = hp.Ring(new_element_list, p0c=p0c_eV)
-p=Particles(e0=26.01692438e9, m0=0.93827205e9)#p0c=ring.p0c)
-n_part = 10
-zeros = np.zeros(n_part)
-p.x=0.001
 
 
 
-
-x = [np.copy(p.x)]
-px = [np.copy(p.px)]
-print('\nstart tracking')
-for t in range(30):
-    print('turn: %03d'%t)
-    ring.track(p)
-    x.append(p.x)
-    px.append(p.px)
-
-f, ax = plt.subplots(1)
-ax.plot(x, px, '.')
-ax.set_xlabel('x (m)')
-ax.set_ylabel('px')
-plt.tight_layout()
-plt.show()
-
-# spstwiss['s'] 
-# print(len(iconv), len(spstwiss['s']))
-'''
-'''
-def get_part(pbench,ii):
-    pstart=[pbench[n][ii] for n in 'x px y py t pt'.split()]
-    pstart=dict(zip('x px y py tau ptau'.split(),pstart))
-    prun=pysixtrack.Particles(energy0=pbench.e[ii]*1e9,**pstart)
-    return prun
-
-def compare(prun,pbench):
-    out=[]
-    for att in 'x px y py tau ptau'.split():
-        vrun=getattr(prun,att)
-        vbench=getattr(pbench,att)
-        diff=vrun-vbench
-        out.append(abs(diff))
-        print(f"{att:<5} {vrun:22.13e} {vbench:22.13e} {diff:22.13g}")
-    print(f"max {max(out):21.12e}")
-    return max(out)
-
-prun=get_part(pbench,0)
-for turn in range(1,30):
-    sps.track(prun)
-    compare(prun,get_part(pbench,turn))
-
-
-
-
-'''
