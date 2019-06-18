eeccb9fb42c7f7ccfcb2ea022dcba1bef3757f45
Riccardo De Maria <riccardodemaria@gmail.com>
2019-06-18 16:19:52 +0200

    new sixtracktools

--- pysixtrack/loader_sixtrack.py
+++ pysixtrack/loader_sixtrack.py
@@ -3,7 +3,7 @@ from math import factorial
 import numpy as np
 
 from .base_classes import Element
-from . import elements 
+from . import elements
 
 clight = 299792458
 pi = np.pi
@@ -20,7 +20,7 @@ def bn_rel(bn16, bn3, r0, d0, sign):
         out.append(bn_mad(sixval, n, sign))
     return out
 
-def _expand_struct(self, convert=elements):
+def _expand_struct(sixinput, convert=elements):
     elems = []
     count = {}
     icount = 0
@@ -37,19 +37,19 @@ def _expand_struct(self, convert=elements):
     BeamBeam6D = convert.BeamBeam6D
     exclude = False
     # add special elenents
-    if 'CAV' in self.iter_struct():
-        self.single['CAV'] = [12*self.ition, self.u0, self.harm, 0]
-    for nnn in self.iter_struct():
+    if 'CAV' in sixinput.iter_struct():
+        sixinput.single['CAV'] = [12*sixinput.ition, sixinput.u0, sixinput.harm, 0]
+    for nnn in sixinput.iter_struct():
         exclude = False
         ccc = count.setdefault(nnn, 0)
-        if len(self.single[nnn]) == 7:
-            etype, d1, d2, d3, d4, d5, d6 = self.single[nnn]
+        if len(sixinput.single[nnn]) == 7:
+            etype, d1, d2, d3, d4, d5, d6 = sixinput.single[nnn]
         else:
-            etype, d1, d2, d3, = self.single[nnn]
+            etype, d1, d2, d3, = sixinput.single[nnn]
             d4, d5, d6 = None, None, None
         elem = None
-        if nnn in self.align:
-            dx, dy, tilt = self.align[nnn][ccc]
+        if nnn in sixinput.align:
+            dx, dy, tilt = sixinput.align[nnn][ccc]
             tilt = tilt*180e-3/pi
             dx *= 1e-3
             dy *= 1e-3
@@ -78,7 +78,7 @@ def _expand_struct(self, convert=elements):
                 knl, ksl = ksl, knl
             elem = Multipole(knl=knl, ksl=ksl, hxl=0, hyl=0, length=0)
         elif etype == 11:
-            knl, ksl = self.get_knl(nnn, ccc)
+            knl, ksl = sixinput.get_knl(nnn, ccc)
             hxl = 0
             hyl = 0
             l = 0
@@ -94,27 +94,27 @@ def _expand_struct(self, convert=elements):
                 ksl[0] = hyl
             elem = Multipole(knl=knl, ksl=ksl, hxl=hxl, hyl=hyl, length=l)
         elif etype == 12:
-            # e0=self.initialconditions[-1]
-            # p0c=np.sqrt(e0**2-self.pma**2)
+            # e0=sixinput.initialconditions[-1]
+            # p0c=np.sqrt(e0**2-sixinput.pma**2)
             # beta0=p0c/e0
             v = d1*1e6
-            freq = d2*clight/self.tlen
+            freq = d2*clight/sixinput.tlen
             # print(v,freq)
             elem = Cavity(voltage=v, frequency=freq, lag=180-d3)
         elif etype == 20:
-            thisbb = self.bbelements[nnn]
-            if type(thisbb) is self.classes['BeamBeam4D']:
+            thisbb = sixinput.bbelements[nnn]
+            if hasattr(thisbb,"sigma_x"):
                 elem = BeamBeam4D(**thisbb._asdict())
-            elif type(thisbb) is self.classes['BeamBeam6D']:
+            elif hasattr(thisbb,"phi"):
                 elem = BeamBeam6D(**thisbb._asdict())
             else:
                 raise ValueError('What?!')
         else:
-            rest.append([nnn]+self.single[nnn])
+            rest.append([nnn]+sixinput.single[nnn])
         if elem is not None:
             elems.append(elem)
             names.append(nnn)
-        if nnn in self.align:
+        if nnn in sixinput.align:
             if hastilt:
                 names.append(nnn+'_posttilt')
                 elems.append(SRotation(angle=-tilt))
