d1212a7350c6aaa037d95a4caa4daf9a637650f4
giadarol <giovanni.iadarola@cern.ch>
2019-06-19 12:54:45 +0200

    Working on mad bb

--- examples/load_from_mad/001_setup_bb.py
+++ examples/load_from_mad/001_setup_bb.py
@@ -37,22 +37,29 @@ class MadPoint(object):
 
 def get_bb_names_and_xyz_points(mad, seq_name):
     mad.use(sequence=seq_name);
-    mad.twiss()
-    mad.survey()
+#    mad.twiss()
+#    mad.survey()
     
     seq = mad.sequence[seq_name]
-    
+   
+    bb_elements = []
     bb_names = []
     bb_xyz_points = []
     for ee in seq.elements:
         if ee.base_type.name == 'beambeam':
             eename = ee.name
+            bb_elements.append(ee)
             bb_names.append(eename)
-            bb_xyz_points.append(MadPoint(eename+':1', mad))
+#            bb_xyz_points.append(MadPoint(eename+':1', mad))
     
-    return bb_names, bb_xyz_points
+    return bb_elements, bb_names, bb_xyz_points
 
-
+def add_beambeam(mad,sequence,name,s,from_):
+    el=mad.command.beambeam.clone(name)
+    mad.seqedit(sequence=sequence)
+    mad.install(element=name,at=s,from_=from_)
+    mad.endedit()
+    return mad.elements[name]
 
 from cpymad.madx import Madx
 import pysixtrack
@@ -64,13 +71,30 @@ mad.options.info=False;
 
 mad.call('mad/lhcwbb.seq')
 
-bb_names_b1, bb_xyz_b1 = get_bb_names_and_xyz_points(mad, seq_name='lhcb1')
-bb_names_b2, bb_xyz_b2 = get_bb_names_and_xyz_points(mad, seq_name='lhcb2')
+
+bb_ele_b1, bb_names_b1, bb_xyz_b1 = get_bb_names_and_xyz_points(mad, seq_name='lhcb1')
+
+# Install BB in B2
+for bb1 in bb_ele_b1:
+    mad.seqedit(sequence='lhcb2')
+    newbb=mad.command.beambeam.clone(bb1.name)
+    mad.install(element=bb1.name, at=bb1.at,from_=bb1['from'],
+            sigx=1e-3, sigy=1e-3, xma=0., yma=0, charge=0., 
+            comments=bb1.comments)
+    mad.endedit()
+
+#bb_ele_b1, bb_names_b1, bb_xyz_b1 = get_bb_names_and_xyz_points(mad, seq_name='lhcb1')
+bb_ele_b2, bb_names_b2, bb_xyz_b2 = get_bb_names_and_xyz_points(mad, seq_name='lhcb2')
 
 # A check
+assert len(bb_names_b1)==len(bb_names_b2)
 for nbb1, nbb2 in zip(bb_names_b1, bb_names_b2):
     assert(nbb1==nbb2)
 
+
+i_bb = 10
+pb1 = bb_xyz_b1[i_bb]
+pb2 = bb_xyz_b2[i_bb]
 
 
 
