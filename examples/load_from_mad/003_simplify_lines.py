0f0d12e9da708d7b6037591ce2d6788a36f6bfeb
giadarol <giovanni.iadarola@cern.ch>
2019-06-22 09:50:23 +0200

    Some naming change

--- examples/load_from_mad/003_simplify_lines.py
+++ examples/load_from_mad/003_simplify_lines.py
@@ -5,14 +5,17 @@ import pysixtrack
 import sixtracktools
 
 with open('line_from_mad.pkl', 'rb') as fid:
-    line_from_mad = pysixtrack.Line.from_dict(pickle.load(fid))
+    lmad = pysixtrack.Line.from_dict(pickle.load(fid))
 
 sixinput = sixtracktools.sixinput.SixInput('sixtrack/')
-line_from_sixtrack, other = pysixtrack.Line.from_sixinput(sixinput)
+lsix, other = pysixtrack.Line.from_sixinput(sixinput)
 
-assert((line_from_mad.get_length() - line_from_sixtrack.get_length())<1e-6)
+assert((lmad.get_length() - lsix.get_length())<1e-6)
 
-for ll in (line_from_mad, line_from_sixtrack):
+for ll in (lmad, lsix):
     ll.remove_inactive_multipoles(inplace=True)
     ll.remove_zero_length_drifts(inplace=True)
     ll.merge_consecutive_drifts(inplace=True)
+
+for ii, (eem, ees) in enumerate(zip(lmad.elements, lsix.elements)):
+    assert(type(eem) == type(ees))
