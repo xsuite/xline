86a8e4c8b3b06237ea485689cf5d0fc6daf7c50a
giadarol <giovanni.iadarola@cern.ch>
2019-06-23 01:16:58 +0200

    Autopep8

--- examples/load_from_mad/001_line_with_bb_from_madx.py
+++ examples/load_from_mad/001_line_with_bb_from_madx.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
@@ -6,8 +7,8 @@ import pysixtrack
 
 from pysixtrack.be_beambeam.tools import norm, find_alpha_and_phi
 from pysixtrack.be_beambeam.tools import get_bb_names_madpoints_sigmas
-from pysixtrack.be_beambeam.tools import shift_strong_beam_based_on_close_ip 
-from pysixtrack.be_beambeam.tools import setup_beam_beam_in_line 
+from pysixtrack.be_beambeam.tools import shift_strong_beam_based_on_close_ip
+from pysixtrack.be_beambeam.tools import setup_beam_beam_in_line
 from pysixtrack import MadPoint
 
 ip_names = [1, 2, 5, 8]
@@ -16,10 +17,10 @@ ip_names = [1, 2, 5, 8]
 n_slices = 11
 
 # Use cpymad to compute the required beam parameters
-mad=Madx()
-mad.options.echo=False;
-mad.options.warn=False;
-mad.options.info=False;
+mad = Madx()
+mad.options.echo = False
+mad.options.warn = False
+mad.options.info = False
 # Sequences (b1 and b2) with clean machine to compute beam-beam parameters
 mad.call('mad/lhcwbb.seq')
 
@@ -27,22 +28,26 @@ mad.call('mad/lhcwbb.seq')
 mad.globals.on_bb_charge = 0.
 
 # Get IP locations from the survey
-mad.use('lhcb1'); mad.twiss(); mad.survey()
+mad.use('lhcb1')
+mad.twiss()
+mad.survey()
 IP_xyz_b1 = {}
 for ip in ip_names:
-    IP_xyz_b1[ip] = MadPoint('ip%d'%ip+':1', mad, add_CO=False)
+    IP_xyz_b1[ip] = MadPoint('ip%d' % ip+':1', mad, add_CO=False)
 
-mad.use('lhcb2'); mad.twiss(); mad.survey()
+mad.use('lhcb2')
+mad.twiss()
+mad.survey()
 IP_xyz_b2 = {}
 for ip in ip_names:
-    IP_xyz_b2[ip] = MadPoint('ip%d'%ip+':1', mad, add_CO=False)
+    IP_xyz_b2[ip] = MadPoint('ip%d' % ip+':1', mad, add_CO=False)
 
 # Get locations of the bb encounters (absolute from survey), closed orbit
 # and orientation of the local reference system (MadPoint objects)
 bb_names_b1, bb_xyz_b1, bb_sigmas_b1 = get_bb_names_madpoints_sigmas(
-        mad, seq_name='lhcb1')
+    mad, seq_name='lhcb1')
 bb_names_b2, bb_xyz_b2, bb_sigmas_b2 = get_bb_names_madpoints_sigmas(
-        mad, seq_name='lhcb2')
+    mad, seq_name='lhcb2')
 
 # Get bunch paramenters
 bunch_intensity = mad.sequence.lhcb1.beam.npart
@@ -50,86 +55,83 @@ gamma_r = mad.sequence.lhcb1.beam.gamma
 beta_r = np.sqrt(1-1./gamma_r**2)
 
 # Check naming convention
-assert len(bb_names_b1)==len(bb_names_b2)
+assert len(bb_names_b1) == len(bb_names_b2)
 for nbb1, nbb2 in zip(bb_names_b1, bb_names_b2):
-    assert(nbb1==nbb2.replace('b2_','b1_'))
+    assert(nbb1 == nbb2.replace('b2_', 'b1_'))
 
 # Check number of slices
 assert(len(
-    [nn for nn in bb_names_b1 if nn.startswith('bb_ho.l1')])==(n_slices-1)/2)
+    [nn for nn in bb_names_b1 if nn.startswith('bb_ho.l1')]) == (n_slices-1)/2)
 
 # Correct for small shifts between surveys of the two beams
 shift_strong_beam_based_on_close_ip(
-        points_weak=bb_xyz_b1, 
-        points_strong=bb_xyz_b2,
-        IPs_survey_weak=IP_xyz_b1,
-        IPs_survey_strong=IP_xyz_b2)
+    points_weak=bb_xyz_b1,
+    points_strong=bb_xyz_b2,
+    IPs_survey_weak=IP_xyz_b1,
+    IPs_survey_strong=IP_xyz_b2)
 
-# Use another instance of cpymad to load sequence to be used from tracking 
+# Use another instance of cpymad to load sequence to be used from tracking
 # (dispesion knowb ON, multipole error correction, tunes, chromaticity)
 mad_ft = Madx()
-mad_ft.options.echo=False;
-mad_ft.options.warn=False;
-mad_ft.options.info=False;
+mad_ft.options.echo = False
+mad_ft.options.warn = False
+mad_ft.options.info = False
 mad_ft.call('mad/lhcwbb_fortracking.seq')
-mad_ft.use('lhcb1') # without this the sequence does not work properly
+mad_ft.use('lhcb1')  # without this the sequence does not work properly
 
 # Build pysixtrack line
 line_for_tracking, _ = pysixtrack.Line.from_madx_sequence(
-        mad_ft.sequence['lhcb1'])
+    mad_ft.sequence['lhcb1'])
 
 # Setup 4D and 6D beam beam lenses
 setup_beam_beam_in_line(line_for_tracking, bb_names_b1,
-        bb_sigmas_strong=bb_sigmas_b2,
-        bb_points_weak=bb_xyz_b1, bb_points_strong=bb_xyz_b2,
-        beta_r_strong = beta_r, bunch_intensity_strong=bunch_intensity, 
-        n_slices_6D=n_slices, bb_coupling=False)
+                        bb_sigmas_strong=bb_sigmas_b2,
+                        bb_points_weak=bb_xyz_b1, bb_points_strong=bb_xyz_b2,
+                        beta_r_strong=beta_r, bunch_intensity_strong=bunch_intensity,
+                        n_slices_6D=n_slices, bb_coupling=False)
 
 # A check
-assert(np.abs(line_for_tracking.get_length()\
-        - mad.sequence.lhcb1.beam.circ)<1e-6)
+assert(np.abs(line_for_tracking.get_length()
+              - mad.sequence.lhcb1.beam.circ) < 1e-6)
 
-# There is a problem in the mask 
+# There is a problem in the mask
 # (the RF frequancy is wrong in the machine for tracking
 # I patch it here -> to be fixed properly!!!!
-line_temp, _ = pysixtrack.Line.from_madx_sequence(mad.sequence.lhcb1) 
+line_temp, _ = pysixtrack.Line.from_madx_sequence(mad.sequence.lhcb1)
 dct_correct_cavities = dict(zip(*line_temp.get_elements_of_type(
     pysixtrack.elements.Cavity)[::-1]))
 for ii, nn in enumerate(line_for_tracking.element_names):
     if nn in dct_correct_cavities.keys():
         line_for_tracking.elements[ii].frequency = \
-                dct_correct_cavities[nn].frequency
+            dct_correct_cavities[nn].frequency
 
 # Save line
 with open('line_from_mad.pkl', 'wb') as fid:
     pickle.dump(line_for_tracking.to_dict(keepextra=True), fid)
 
 # Plot positions and local reference system at bb interactions
-import matplotlib.pyplot as plt
 plt.close('all')
 fig1 = plt.figure(1)
 
-plt.plot(           [pb.p[2] for pb in bb_xyz_b1],
-                    [pb.p[0] for pb in bb_xyz_b1], 'b.')
+plt.plot([pb.p[2] for pb in bb_xyz_b1],
+         [pb.p[0] for pb in bb_xyz_b1], 'b.')
 plt.quiver(np.array([pb.p[2] for pb in bb_xyz_b1]),
-           np.array([pb.p[0] for pb in bb_xyz_b1]), 
-          np.array([pb.ex[2] for pb in bb_xyz_b1]),
-          np.array([pb.ex[0] for pb in bb_xyz_b1]))
+           np.array([pb.p[0] for pb in bb_xyz_b1]),
+           np.array([pb.ex[2] for pb in bb_xyz_b1]),
+           np.array([pb.ex[0] for pb in bb_xyz_b1]))
 plt.quiver(np.array([pb.p[2] for pb in bb_xyz_b1]),
-           np.array([pb.p[0] for pb in bb_xyz_b1]), 
-          np.array([pb.ez[2] for pb in bb_xyz_b1]),
-          np.array([pb.ez[0] for pb in bb_xyz_b1]))
+           np.array([pb.p[0] for pb in bb_xyz_b1]),
+           np.array([pb.ez[2] for pb in bb_xyz_b1]),
+           np.array([pb.ez[0] for pb in bb_xyz_b1]))
 
-plt.plot(           [pb.p[2] for pb in bb_xyz_b2],
-                    [pb.p[0] for pb in bb_xyz_b2], 'r.')
+plt.plot([pb.p[2] for pb in bb_xyz_b2],
+         [pb.p[0] for pb in bb_xyz_b2], 'r.')
 plt.quiver(np.array([pb.p[2] for pb in bb_xyz_b2]),
-           np.array([pb.p[0] for pb in bb_xyz_b2]), 
-          np.array([pb.ex[2] for pb in bb_xyz_b2]),
-          np.array([pb.ex[0] for pb in bb_xyz_b2]))
+           np.array([pb.p[0] for pb in bb_xyz_b2]),
+           np.array([pb.ex[2] for pb in bb_xyz_b2]),
+           np.array([pb.ex[0] for pb in bb_xyz_b2]))
 plt.quiver(np.array([pb.p[2] for pb in bb_xyz_b2]),
-           np.array([pb.p[0] for pb in bb_xyz_b2]), 
-          np.array([pb.ez[2] for pb in bb_xyz_b2]),
-          np.array([pb.ez[0] for pb in bb_xyz_b2]))
+           np.array([pb.p[0] for pb in bb_xyz_b2]),
+           np.array([pb.ez[2] for pb in bb_xyz_b2]),
+           np.array([pb.ez[0] for pb in bb_xyz_b2]))
 plt.show()
-
-
--- examples/load_from_mad/002_compare_against_sixinput.py
+++ examples/load_from_mad/002_compare_against_sixinput.py
@@ -12,7 +12,7 @@ sixinput = sixtracktools.sixinput.SixInput('sixtrack/')
 lsix, other = pysixtrack.Line.from_sixinput(sixinput)
 
 original_length = lmad.get_length()
-assert((lsix.get_length() - original_length)<1e-6)
+assert((lsix.get_length() - original_length) < 1e-6)
 
 # Simplify the two machines
 for ll in (lmad, lsix):
@@ -23,44 +23,46 @@ for ll in (lmad, lsix):
 # Check that the two machines are identical
 assert(len(lmad) == len(lsix))
 
-assert((lmad.get_length() - original_length)<1e-6)
-assert((lsix.get_length() - original_length)<1e-6)
+assert((lmad.get_length() - original_length) < 1e-6)
+assert((lsix.get_length() - original_length) < 1e-6)
+
 
 def norm(x):
     return np.sqrt(np.sum(np.array(x)**2))
 
+
 for ii, (ee_mad, ee_six, nn_mad, nn_six) in enumerate(zip(
-    lmad.elements, lsix.elements, lmad.element_names, lsix.element_names)):
+        lmad.elements, lsix.elements, lmad.element_names, lsix.element_names)):
     assert(type(ee_mad) == type(ee_six))
 
     dmad = ee_mad.to_dict(keepextra=True)
     dsix = ee_six.to_dict(keepextra=True)
 
     for kk in dmad.keys():
-        
+
         # Check if they are identical
         if dmad[kk] == dsix[kk]:
             continue
-        
+
         # Check if the relative error is small
         try:
             diff_rel = norm(
-                    np.array(dmad[kk]) - np.array(dsix[kk]))/norm(dmad[kk])
+                np.array(dmad[kk]) - np.array(dsix[kk]))/norm(dmad[kk])
         except ZeroDivisionError:
             diff_rel = 100.
-        if diff_rel < 3e-5: 
+        if diff_rel < 3e-5:
             continue
 
         # Check if absolute error is small
         diff_abs = norm(np.array(dmad[kk]) - np.array(dsix[kk]))
-        if diff_abs>0:
+        if diff_abs > 0:
             print(f"{nn_mad}[{kk}] - mad:{dmad[kk]} six:{dsix[kk]}")
         if diff_abs < 1e-12:
             continue
 
         # Exception: drift length (100 um tolerance)
         if isinstance(ee_mad, (pysixtrack.elements.Drift,
-            pysixtrack.elements.DriftExact)):
+                               pysixtrack.elements.DriftExact)):
             if kk == 'length':
                 if diff_abs < 1e-4:
                     continue
@@ -78,16 +80,16 @@ for ii, (ee_mad, ee_six, nn_mad, nn_six) in enumerate(zip(
             if kk == 'y_bb':
                 if diff_abs/dmad['sigma_y'] < 0.0001:
                     continue
-        
+
         # Exceptions BB4D (angles and separations are recalculated)
         if isinstance(ee_mad, pysixtrack.elements.BeamBeam6D):
-            if kk=='alpha':
-                if diff_abs<10e-6:
+            if kk == 'alpha':
+                if diff_abs < 10e-6:
                     continue
-            if kk=='x_bb_co':
+            if kk == 'x_bb_co':
                 if diff_abs/np.sqrt(dmad['sigma_11']) < 0.001:
                     continue
-            if kk=='y_bb_co':
+            if kk == 'y_bb_co':
                 if diff_abs/np.sqrt(dmad['sigma_33']) < 0.001:
                     continue
 
