1f461ba51704c1b47c13cd3d6019b82f506e6ef6
Riccardo De Maria <riccardodemaria@gmail.com>
2019-11-05 10:50:46 +0100

    fix setup.py and some tests

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,12 +1,13 @@
 from pysixtrack import elements
 
 
-el = elements.Drift()
-el = elements.DriftExact()
-el = elements.Multipole()
-assert el.order == 0
-el = elements.XYShift()
-el = elements.SRotation()
-el = elements.Cavity()
-el = elements.Line()
-el = elements.DipoleEdge()
+def test_element_creation():
+   el = elements.Drift()
+   el = elements.DriftExact()
+   el = elements.Multipole()
+   assert el.order == 0
+   el = elements.XYShift()
+   el = elements.SRotation()
+   el = elements.Cavity()
+   el = elements.Line()
+   el = elements.DipoleEdge()
