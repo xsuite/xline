360d2aa30a58cfa439b1aecde2ef963175e725c0
Riccardo De Maria <riccardodemaria@gmail.com>
2019-11-15 22:31:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 import setuptools
+import sys
 
+requirements={
+    "install": ['numpy']
+}
+ 
+if sys.version_info < (3,7):
+    requirements['install'].append('dataclasses')
+    
 setuptools.setup(
     name="pysixtrack",
     version="0.0.4",
@@ -9,5 +17,5 @@ setuptools.setup(
     url="https://github.com/rdemaria/pysixtrack",
     packages=["pysixtrack", "pysixtrack.be_beamfields"],
     package_dir={"pysixtrack": "pysixtrack"},
-    install_requires=["numpy"],
+    install_requires=requirements['install'],
 )
