f56c0c847fa192da361ce4c2049eaf35bb25bd24
Riccardo De Maria <riccardodemaria@gmail.com>
2019-11-15 22:42:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ requirements={
 if sys.version_info < (3,7):
     requirements['install'].append('dataclasses')
     
+version = open("pysixtrack/__init__.py").readline().split('=')[1].strip()
+    
 setuptools.setup(
     name="pysixtrack",
-    version="0.0.4",
+    version=version,
     description="6D Tracking Code",
     author="Riccardo De Maria",
     author_email="riccardo.de.maria@cern.ch",
