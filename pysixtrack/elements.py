07175eed36b83606fc64f28ff2a2832e3821f75a
Hannes Bartosik <hannes.bartosik@cern.ch>
2019-11-29 10:56:42 +0100

    Fix aperture check

--- pysixtrack/elements.py
+++ pysixtrack/elements.py
@@ -319,7 +319,7 @@ class LimitRect(Element):
                 & (y <= self.max_y)
             )
             particle.remove_lost_particles()
-            if len(particle.state == 0):
+            if len(particle.state) == 0:
                 return -1
 
 
@@ -345,7 +345,7 @@ class LimitEllipse(Element):
                 x * x / (self.a * self.a) + y * y / (self.b * self.b) <= 1.0
             )
             particle.remove_lost_particles()
-            if len(particle.state == 0):
+            if len(particle.state) == 0:
                 return -1
 
 
