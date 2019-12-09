37cd97e5f084397cc00f645165540c69b08b2aed
giadarol <giovanni.iadarola@cern.ch>
2019-12-09 13:13:59 +0100

    Tests and some fixes

--- tests/test_track.py
+++ tests/test_track.py
@@ -13,6 +13,8 @@ element_list = [
     pysixtrack.elements.Line,
     pysixtrack.elements.LimitRect,
     pysixtrack.elements.LimitEllipse,
+    pysixtrack.elements.BeamBeam4D,
+    pysixtrack.elements.BeamBeam6D,
 ]
 
 
