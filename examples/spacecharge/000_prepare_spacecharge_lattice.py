7f2e887c54c8cf0544278da3e6dd9e27bfc7b588
Hannes Bartosik <hannes.bartosik@cern.ch>
2020-01-06 17:50:40 +0100

    Lintering

--- examples/spacecharge/000_prepare_spacecharge_lattice.py
+++ examples/spacecharge/000_prepare_spacecharge_lattice.py
@@ -12,11 +12,11 @@ import pysixtrack.be_beamfields.tools as bt
 # sc_mode = 'Coasting'
 sc_mode = "Bunched"
 
-particle_type = 'protons'
+particle_type = "protons"
 # particle_type = 'ions'
 
-if particle_type == 'protons':
-    
+if particle_type == "protons":
+
     # For bunched
     number_of_particles = 2e11
     bunchlength_rms = 0.22
@@ -24,9 +24,9 @@ if particle_type == 'protons':
     # For coasting
     line_density = 2e11 / 0.5
 
-    mass = pysixtrack.Particles.pmass 
+    mass = pysixtrack.Particles.pmass
     p0c = 25.92e9
-    charge_state = 1.
+    charge_state = 1.0
     neps_x = 2e-6
     neps_y = 2e-6
     delta_rms = 1.5e-3
@@ -35,8 +35,8 @@ if particle_type == 'protons':
     n_SCkicks = 108
     length_fuzzy = 1.5
 
-if particle_type == 'ions':    
-    
+if particle_type == "ions":
+
     # For bunched
     number_of_particles = 3.5e8
     bunchlength_rms = 0.22
@@ -44,9 +44,9 @@ if particle_type == 'ions':
     # For coasting
     line_density = 3.5e8 / 0.5
 
-    mass = 193.7e9 
+    mass = 193.7e9
     p0c = 1402.406299e9
-    charge_state = 82.
+    charge_state = 82.0
     neps_x = 1.63e-6
     neps_y = 0.86e-6
     delta_rms = 1.0e-3
