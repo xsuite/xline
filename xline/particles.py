221d79ebd5515c26731fe1d788a70cd12816c547
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-02 16:04:53 +0100

    Going back to python particle, xpart too slow for xline

--- xline/particles.py
+++ xline/particles.py
@@ -1,17 +1,6 @@
 import xpart as xp
 from .mathlibs import MathlibDefault
 
-class XlineTestParticles(xp.Particles):
+class XlineTestParticles(xp.particles._pyparticles.Pyparticles):
 
     _m = MathlibDefault
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.hide_lost_particles()
-        self.delta.flags.writeable=False
-        self.rvv.flags.writeable=False
-        self.rpp.flags.writeable=False
-        self.psigma.flags.writeable=False
-
-    def remove_lost_particles(self):
-        self.reorganize()
